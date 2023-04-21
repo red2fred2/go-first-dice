3e0add045dc63def10fac1eda0c4bc165894cdf7
red2fred2 <red2fred2base@live.com>
2023-04-21 02:54:09 -0500

    Test program on other known good dice

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,39 @@ use dice::Dice;
 
 fn main() {
 	// The original go first dice that are permutation fair for 4 players
-	let go_first_dice = Dice::new(
-		vec![
-			vec![1, 8, 11, 14, 19, 22, 27, 30, 35, 38, 41, 48],
-			vec![2, 7, 10, 15, 18, 23, 26, 31, 34, 39, 42, 47],
-			vec![3, 6, 12, 13, 17, 24, 25, 32, 36, 37, 43, 46],
-			vec![4, 5, 9,  16, 20, 21, 28, 29, 33, 40, 44, 45],
-		]
-	);
-
-	go_first_dice.count_permutations();
-
 	// let dice = Dice::new(
 	// 	vec![
-	// 		vec![1, 3, 5, 7],
-	// 		vec![2, 4, 6, 8],
+	// 		vec![1, 8, 11, 14, 19, 22, 27, 30, 35, 38, 41, 48],
+	// 		vec![2, 7, 10, 15, 18, 23, 26, 31, 34, 39, 42, 47],
+	// 		vec![3, 6, 12, 13, 17, 24, 25, 32, 36, 37, 43, 46],
+	// 		vec![4, 5, 9,  16, 20, 21, 28, 29, 33, 40, 44, 45],
 	// 	]
 	// );
 
-	// dice.count_permutations();
+	// 2 player matched go first dice
+	// let dice = Dice::new(
+	// 	vec![
+	// 		vec![1, 4],
+	// 		vec![2, 3],
+	// 	]
+	// );
+
+	// 2 player mismatched go first dice
+	// let dice = Dice::new(
+	// 	vec![
+	// 		vec![1, 3],
+	// 		vec![2],
+	// 	]
+	// );
+
+	// 3 player matched go first dice
+	let dice = Dice::new(
+		vec![
+			vec![1, 5, 10, 11, 13, 17],
+			vec![3, 4, 7,  12, 15, 16],
+			vec![2, 6, 8,   9, 14, 18],
+		]
+	);
+
+	dice.count_permutations();
 }
